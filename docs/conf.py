8fe3adee8de55a1651dea2eadde6670f8f566216
benja263 <benjafuhrer@gmail.com>
2024-06-03 12:59:09 +0300

    fix doc generation

--- docs/conf.py
+++ docs/conf.py
@@ -40,10 +40,9 @@ extensions = [
     'sphinx.ext.ifconfig',
 ]
 
+nbsphinx_execute = 'never'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
