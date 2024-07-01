5e26353f9e149bdf9e53181274d71e4d18dbc8e5
benja263 <benjafuhrer@gmail.com>
2024-07-01 15:00:30 +0300

    fixed extra ;

--- gbrl/src/cpp/gbrl.cpp
+++ gbrl/src/cpp/gbrl.cpp
@@ -761,7 +761,7 @@ int GBRL::saveToFile(const std::string& filename){
 
     file.close();
     return 0;  // success
-};
+}
 
 int GBRL::loadFromFile(const std::string& filename){
     std::ifstream file(filename, std::ios::binary);
