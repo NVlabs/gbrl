39204603d926b91bcdcede0e5e1794d6be98cbb2
benja263 <benjafuhrer@gmail.com>
2025-03-12 14:52:27 +0200

    fixing fit function

--- gbrl/src/cpp/loss.h
+++ gbrl/src/cpp/loss.h
@@ -13,8 +13,8 @@
 
 class MultiRMSE{
     public:
-        static float get_loss_and_gradients(const float *raw_preds, const float *raw_targets, float *raw_grads, const int n_samples, const int output_dim);
-        static float get_loss(const float *raw_preds, const float *raw_targets, const int n_samples, const int output_dim);
+        static float get_loss_and_gradients(const float *raw_preds, const float *raw_targets, float *raw_grads, const int n_samples, const int output_dim, const int par_th);
+        static float get_loss(const float *raw_preds, const float *raw_targets, const int n_samples, const int output_dim, const int par_th);
 };
 
 
