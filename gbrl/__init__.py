0486883a9b7f040a7f8e3a95a5d92037bc221c6d
benja263 <benjafuhrer@gmail.com>
2026-09-01 10:40:42 +0300

    fixing major version and testing cuda 13

--- gbrl/__init__.py
+++ gbrl/__init__.py
@@ -19,7 +19,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 ##############################################################################
-__version__ = "1.1.9"
+__version__ = "1.1.10"
 
 import importlib.util
 import os
